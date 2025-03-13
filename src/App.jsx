ae97a8df0ebf94048abeb7dd63655bcee0284c9c
Jason <jtimm@gicw.org>
2025-03-13 08:55:02 -0700

    got FilterForm to work

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,21 @@ import Gallery from './Gallery';
 import SelectedBeast from './SelectedBeast';
 import Header from './Header';
 import Footer from './Footer';
+import  FilterForm from './FilterForm';
 import data from './data.json'; // Import JSON data
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
   const [selectedBeast, setSelectedBeast] = useState(null); // Track selected beast
-
+  const [filter, setFilter] = useState("all");
+  const filterData = filter=== "all" ? data : data.filter(beast => beast.horns === Number(filter));
   return (
     <div>
       <Header />
       <h1>Horned Beasts</h1>
-      <Gallery data={data} setSelectedBeast={setSelectedBeast} />
+      <FilterForm setFilter = {setFilter}/>
+      <Gallery data={filterData} setSelectedBeast={setSelectedBeast} />
       {selectedBeast && <SelectedBeast beast={selectedBeast} setSelectedBeast={setSelectedBeast} />}
   <Footer />
     </div>
